1fd22059faef1ab421e81f6f9963d2a04fffcdba
maconrm180 <maconrm180@DESKTOP-AUJG0KN>
2019-09-03 13:50:56 -0500

    new commit

---
+++ src/myhelloworld/MyHelloWorld.java
@@ -0,0 +1,19 @@
+
+package myhelloworld;
+
+
+public class MyHelloWorld {
+
+    
+    public static void main(String[] args) {
+        
+        //This is my first Java Program - Hello World
+        
+        System.out.println("Hello Java Class");
+        
+   
+        
+        
+    }
+    
+}
